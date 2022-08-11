a7e13301a66053de98460d721f922615d2be6723
owao <kori0423@naver.com>
2022-08-11 16:35:52 +0900

    Update Ridge&Lasso Regression.py

--- Ridge&Lasso Regression.py
+++ Ridge&Lasso Regression.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
 
@@ -22,9 +23,12 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
        1000.0])
 
 
-#훈련용 데이터와 테스트 데이터로 분리한 다음 사이킷런에 맞게 열이 2인 행렬로 바꿔준다
+#훈련용 데이터와 테스트 데이터로 분리
 
 train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
+
+
+#특성 공학(feature engineering): 새 특성 만들기, 사이킷런의 변환기(transfomer) 사용
 
 
 #모델 훈련(1차방정식 선형 회귀)
