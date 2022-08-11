40b44ce4d0b338c60869902414163c59b4368a34
owao <kori0423@naver.com>
2022-08-11 16:18:35 +0900

    Update Ridge&Lasso Regression.py

--- Ridge&Lasso Regression.py
+++ Ridge&Lasso Regression.py
@@ -1,19 +1,18 @@
 #혼공머신 교재 3-3챕터
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 
-#제대로 된 농어 데이터
+#pandas를 이용한 데이터 준비
 
-perch_length = np.array([8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0, 21.0,
-       21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5, 22.5, 22.7,
-       23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5, 27.3, 27.5, 27.5,
-       27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0, 36.5, 36.0, 37.0, 37.0,
-       39.0, 39.0, 39.0, 40.0, 40.0, 40.0, 40.0, 42.0, 43.0, 43.0, 43.5,
-       44.0])
+df = pd.read_csv('https://bit.ly/perch_csv_data')
+perch_full = df.to_numpy()
+print(perch_full)
+
 perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 110.0,
        115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 130.0,
        150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 197.0,
@@ -25,27 +24,9 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
 
 #훈련용 데이터와 테스트 데이터로 분리한 다음 사이킷런에 맞게 열이 2인 행렬로 바꿔준다
 
-train_input, test_input, train_target, test_target = train_test_split(perch_length, perch_weight, random_state=42)
-train_input = train_input.reshape(-1, 1)  #42개 데이터. -1은 전체 사이즈대로 행을 유지한다는 뜻
-test_input = test_input.reshape(-1, 1)  #14개 데이터.
+train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42).
 
 
 #모델 훈련(1차방정식 선형 회귀)
 
-lr = LinearRegression()
-lr.fit(train_input, train_target)
-print(lr.coef_, lr.intercept_)  #기울기와 절편
 
-print("1차방정식 선형 회귀 훈련 모델 점수", lr.score(train_input, train_target))
-print("1차방정식 선형 회귀 테스트 모델 점수", lr.score(test_input, test_target))
-
-
-#모델 훈련(다항 회귀, polynomial regression)
-train_poly = np.column_stack((train_input**2, train_input))  #이차식에 적합한 데이터셋 제작
-test_poly = np.column_stack((test_input**2, test_input))  #이차항에 들어가는 변수, 일차항에 들어가는 변수로 데이터셋이 구성됨
-
-lr.fit(train_poly, train_target)
-print(lr.coef_, lr.intercept_)
-
-print("다항 회귀 훈련 모델 점수", lr.score(train_poly, train_target))
-print("다항 회귀 테스트 모델 점수", lr.score(test_poly, test_target))
