721e25816e39373bd2e0ffec8db969288f951e7a
owao <kori0423@naver.com>
2022-08-23 12:18:13 +0900

    feature engneering

--- Ridge&Lasso Regression.py
+++ Ridge&Lasso Regression.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 
 #pandas를 이용한 데이터 준비
@@ -30,6 +31,12 @@ train_input, test_input, train_target, test_target = train_test_split(perch_full
 
 #특성 공학(feature engineering): 새 특성 만들기, 사이킷런의 변환기(transfomer) 사용
 
+poly = PolynomialFeatures(include_bias=False) #사이킷런의 선형 모델이 자동으로 절편을 추가할 것이므로 절편 특성 추가는 필요 없음
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+
+poly.get_feature_names_out() #특성이 어떻게 만들어졌는지 확인(어떤 입력 조합인지)
+test_poly = poly.transform(test_input)
 
 #모델 훈련(1차방정식 선형 회귀)
 
