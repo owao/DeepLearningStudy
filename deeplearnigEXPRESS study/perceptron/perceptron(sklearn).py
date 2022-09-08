3c611bae5e948105f1fc0ad6ad968c432abaa3de
owao <kori0423@naver.com>
2022-09-08 10:45:05 +0900

    perceptron study

---
+++ deeplearnigEXPRESS study/perceptron/perceptron(sklearn).py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import Perceptron
+
+X = [[0,0],[0,1],[1,0],[1,1]]
+y = [0, 0, 0, 1]
+
+clf = Perceptron(tol=1e-3, random_state=0) #tol: 종료 조건
+clf.fit(X,y)
+
+prit(clf.predict(X))
